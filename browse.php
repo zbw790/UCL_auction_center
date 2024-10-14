d0f15c2248113f5245a7e54466808e0bc851312c
zbw790 <1018980349@qq.com>
2024-10-14 02:14:18 +0100

    Update browse.php

--- browse.php
+++ browse.php
@@ -301,7 +301,7 @@ if (!isset($_SESSION['logged_in'])) {
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
         <h5 class="text-uppercase">About Us</h5>
         <p>
-          We are a leading online auction platform, connecting buyers and sellers from around the world.
+          UCL is the best!!!!.
         </p>
       </div>
       <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
