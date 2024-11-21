12e5c127cba3a60893ea60cd111e5fd9d01dc30f
kygagagu <101474447+kyy0809@users.noreply.github.com>
2024-11-22 00:01:46 +0800

    auto-time-countdown is set
    new auctions may can insert into the DB (waiting for the structure of create_auction.php), and auto-time-countdown is set.

--- create_auction_result.php
+++ create_auction_result.php
@@ -1,33 +1,35 @@
-<?php include_once("header.php")?>
+<?php include_once("header.php");
+require_once('db_connect.php'); ?>
 
 <div class="container my-5">
 
-<?php
+    <?php
+    // 提取表单数据
+    $item_name = $_POST['auctionTitle'];
+    $description = $_POST['auctionDetails'];
+    $category_id = $_POST['auctionCategory'];
+    $starting_price = $_POST['auctionStartPrice'];
+    $reserve_price = isset($_POST['auctionReservePrice']) ? $_POST['auctionReservePrice'] : null;
+    $end_date = $_POST['auctionEndDate'];
 
-// This function takes the form data and adds the new auction to the database.
+    // 设置拍卖开始时间
+    $start_date = new DateTime();
 
-/* TODO #1: Connect to MySQL database (perhaps by requiring a file that
-            already does this). */
+    // 插入拍卖数据到 auction 表
+    try {
+        $stmt = $pdo->prepare("
+        INSERT INTO auction (seller_id, item_name, description, category_id, start_date, end_date, starting_price, reserve_price, status) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'active')
+    ");
+        $stmt->execute([$_SESSION['user_id'], $item_name, $description, $category_id, $start_date->format('Y-m-d H:i:s'), $end_date, $starting_price, $reserve_price]);
 
-
-/* TODO #2: Extract form data into variables. Because the form was a 'post'
-            form, its data can be accessed via $POST['auctionTitle'], 
-            $POST['auctionDetails'], etc. Perform checking on the data to
-            make sure it can be inserted into the database. If there is an
-            issue, give some semi-helpful feedback to user. */
-
-
-/* TODO #3: If everything looks good, make the appropriate call to insert
-            data into the database. */
-            
-
-// If all is successful, let user know.
-echo('<div class="text-center">Auction successfully created! <a href="FIXME">View your new listing.</a></div>');
-
-
-?>
+        // 显示成功消息
+        echo ('<div class="text-center">Auction successfully created! <a href="listing.php?auction_id=' . $pdo->lastInsertId() . '">View your new listing.</a></div>');
+    } catch (PDOException $e) {
+        echo ('<div class="alert alert-danger">Failed to create auction: ' . $e->getMessage() . '</div>');
+    }
+    ?>
 
 </div>
 
-
-<?php include_once("footer.php")?>
+<?php include_once("footer.php"); ?>
